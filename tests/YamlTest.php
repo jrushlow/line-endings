804d231d8a058ba2bd2495bbfc0b89992da4555e
Jesse Rushlow <jr@rushlow.dev>
2022-07-27 15:26:47 -0400

    simple yaml test

---
+++ tests/YamlTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Jrushlow\LineEndings\Test;
+
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Yaml\Yaml;
+
+class YamlTest extends TestCase
+{
+    public function testYamlFile(): void
+    {
+        $yaml = [
+            'security' => [
+                'firewalls' => [
+                    'main' => [
+                        'custom_authenticator' => 'App\Security\SomeOtherAuthenticator',
+                    ],
+                ],
+            ],
+        ];
+
+        $newContents = Yaml::dump($yaml, 100);
+
+        self::assertSame($this->getYaml(), $newContents);
+    }
+
+    private function getYaml(): string
+    {
+        return <<< 'EOT'
+security:
+    firewalls:
+        main:
+            custom_authenticator: App\Security\SomeOtherAuthenticator
+
+EOT;
+
+    }
+}
