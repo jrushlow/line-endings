57efab063135a1d027540d6fef05e8290d82201e
Jesse Rushlow <jr@rushlow.dev>
2022-07-27 15:08:16 -0400

    write contents to file and compare result

--- tests/StringTest.php
+++ tests/StringTest.php
@@ -6,6 +6,20 @@ use PHPUnit\Framework\TestCase;
 
 class StringTest extends TestCase
 {
+    private const TMP_PATH = __DIR__.'/tmp';
+
+    protected function setUp(): void
+    {
+        if (!is_dir(self::TMP_PATH)) {
+            mkdir(self::TMP_PATH);
+        }
+    }
+
+    protected function tearDown(): void
+    {
+//        rmdir(self::TMP_PATH);
+    }
+
     public function testTest(): void
     {
         self::assertTrue(true);
@@ -19,6 +33,16 @@ class StringTest extends TestCase
     public function testStringSameAsFile(): void
     {
         self::assertSame($this->getString(), file_get_contents(__DIR__.'/fixtures/string_line.test'));
+    }
+
+    public function testWriteStringToFile(): void
+    {
+        file_put_contents(self::TMP_PATH.'/test-1.result', $this->getString());
+
+        self::assertSame(
+            file_get_contents(__DIR__.'/fixtures/string_line.test'),
+            file_get_contents(self::TMP_PATH.'/test-1.result')
+        );
     }
 
     private function getString(): string
