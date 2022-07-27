a2ca9c0b7e41ad2e71c7fb6eaf5849715fab46af
Jesse Rushlow <jr@rushlow.dev>
2022-07-27 14:58:32 -0400

    string test

--- tests/StringTest.php
+++ tests/StringTest.php
@@ -10,4 +10,42 @@ class StringTest extends TestCase
     {
         self::assertTrue(true);
     }
+
+    public function testStringEqualsFile(): void
+    {
+        self::assertEquals($this->getString(), file_get_contents(__DIR__.'/fixtures/string_line.test'));
+    }
+
+    public function testStringSameAsFile(): void
+    {
+        self::assertSame($this->getString(), file_get_contents(__DIR__.'/fixtures/string_line.test'));
+    }
+
+    private function getString(): string
+    {
+        return <<< 'EOT'
+security:
+    firewalls:
+        main:
+            custom_authenticator: App\Security\SomeOtherAuthenticator
+
+
+
+===
+$string = $data['security']['firewalls']['main']['custom_authenticator'];
+$data['security']['firewalls']['main']['custom_authenticator'] = [];
+$data['security']['firewalls']['main']['custom_authenticator'][] = $string;
+$data['security']['firewalls']['main']['custom_authenticator'][] = 'App\Security\AppCustomAuthenticator';
+===
+security:
+    firewalls:
+        main:
+            custom_authenticator:
+                - App\Security\SomeOtherAuthenticator
+                - App\Security\AppCustomAuthenticator
+
+
+
+EOT;
+    }
 }
